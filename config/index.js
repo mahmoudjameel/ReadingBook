febd35e567ae88c4083c4b08a0d4a938c5467118
MahmoudOs <mahmoudos@MahmoudOss-MacBook-Pro.local>
2023-01-02 13:55:19 +0200

    first commit

---
+++ config/index.js
@@ -0,0 +1,7 @@
+export const BASE_URL = 'https://booktest.herokuapp.com//api/Books';
+
+export const CHAPTER_URL = 'https://booktest.herokuapp.com/api/Chapters';
+export const LOGIN_URL = 'https://booktest.herokuapp.com/api/login';
+export const GENRE_URL = 'https://booktest.herokuapp.com/api/Genres';
+
+export const SIGNUP_URL = 'https://booktest.herokuapp.com/api/register';
